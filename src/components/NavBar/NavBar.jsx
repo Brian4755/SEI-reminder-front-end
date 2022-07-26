6cc19930758827155547e9fd1d810ec5fa50b6e9
Brian4755 <brian.mccune1@yahoo.com>
2022-07-25 19:14:15 -0500

    navbar base setup

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,27 +1,41 @@
 import { Link } from 'react-router-dom'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import './NavBar.css'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-          </ul>
-        </nav>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                SEI 5/23
+              </Typography>
+              <Button  color="inherit"><Link to="/reminders">Personal Reminders</Link></Button>
+              <Button  color="inherit"><Link to="" onClick={handleLogout}>Log out</Link></Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
       :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
-      }
-    </>
-  )
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                SEI 5/23
+              </Typography>
+              <Button color="inherit"><Link to="/signup">Sign Up</Link></Button>
+              <Button color="inherit"><Link to="/login">Log In</Link></Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
+      } 
+      </>
+  );
 }
 
 export default NavBar
